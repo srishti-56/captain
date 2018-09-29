46b2e5e184e52846ffccea8b35e160eaaa99dce7
Srishti Mishra <srishtimishra56@gmail.com>
2018-09-30 02:55:40 +0530

    initialized simple Docker setup with Flask app

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,11 +1,15 @@
-from flask import Flask, jsonify
+import sys
+import os
 
+from flask import Flask, jsonify
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)  
+#print(app.config, file=sys.stderr)
 
 
 @app.route('/users/ping', methods=['GET'])
