acb049eaf7e3decf1c2140846b97c9f4e113c953
Srishti Mishra <srishtimishra56@gmail.com>
2018-09-30 01:15:25 +0530

    initialized simple Flask application with starter code

---
+++ services/users/manage.py
@@ -0,0 +1,10 @@
+from flask.cli import FlaskGroup
+
+from project import app
+
+# creates a new FlaskGroup instance to extend the normal CLI with commands related to the Flask app
+cli = FlaskGroup(app)
+
+
+if __name__ == '__main__':
+	cli()
